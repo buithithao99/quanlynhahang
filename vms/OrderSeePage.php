647258b6038c449de0fe2d10502d8f22a47f75f8
buithithao99 <buithithao99meomeo@gmail.com>
2021-04-19 00:12:27 +0700

    Fix see order detail

--- vms/OrderSeePage.php
+++ vms/OrderSeePage.php
@@ -5,6 +5,7 @@ use api\v1\UserAPI;
 use vms\templates\AdminTemplate;
 class OrderSeePage {
     public $rows;
+    public $datas;
     public function __construct($params = null) {
         session_start();
         if(!isset($_SESSION['user_id'])){
@@ -12,6 +13,7 @@ class OrderSeePage {
         }
         $this->title  = "Chi tiết đơn hàng";
         $this->rows = UserAPI::getProductByOrder($params[0]);
+        $this->datas =  UserAPI::getProductByOrder($params[0]);
     }
 
     // Khai báo template và truyền bản thân vào template cha
@@ -26,13 +28,13 @@ class OrderSeePage {
     // Đổi lại tên __render nếu dùng template cha
     public function __render() {
 ?>
-<?php foreach($this->rows->message as $row): ?>
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header">Đơn đặt hàng
                 <small>Chi tiết</small>
             </h1>
         </div>
+        <?php foreach($this->datas->message as $row): ?>
         <!-- /.col-lg-12 -->
         <?php if($_SESSION['type'] === 'admin'): ?>
             <form method="POST" action="/seeorder" style="margin-bottom: 3rem;">
@@ -93,6 +95,8 @@ class OrderSeePage {
                 <p>Số điện thoại: <?= $row['phone'] ?></p>
             </div>
         <?php endif; ?>
+        <?php break; ?>
+        <?php endforeach; ?>
         <table class="table table-striped table-bordered table-hover" id="menu-table">
             <thead>
                 <tr align="center">
@@ -102,13 +106,14 @@ class OrderSeePage {
                 </tr>
             </thead>
             <tbody>
+            <?php foreach($this->rows->message as $row): ?>
                 <tr class="even gradeC" align="center">
                     <th><?= $row['name'] ?></th>
                     <th><?= $row['price'] ?></th>
                     <th><?= $row['order_qty'] ?></th>
                 </tr>
+             <?php endforeach; ?>
             </tbody>
         </table>
     </div>
-<?php endforeach; ?>
 <?php }}
